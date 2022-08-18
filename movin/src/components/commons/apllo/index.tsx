68ecae13c97b00e7e2a7bc6abf5f3299f1be3d98
heejoo45890 <heejoo45890@gmail.com>
2022-08-18 17:05:06 +0900

    :수정: graphQl 주소

--- movin/src/components/commons/apllo/index.tsx
+++ movin/src/components/commons/apllo/index.tsx
@@ -44,7 +44,7 @@ export default function ApolloSetting(props: any) {
   });
 
   const uploadLink = createUploadLink({
-    uri: "https://backend07.codebootcamp.co.kr/graphql",
+    uri: "https://backend07.codebootcamp.co.kr/graphql06",
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
