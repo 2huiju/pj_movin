5d7777fb27c8509b22e5ba6294bba3ab4f9ae2d8
heejoo45890 <heejoo45890@gmail.com>
2022-08-25 11:58:53 +0900

    :수정: error

--- movin/pages/product/[Product]/index.tsx
+++ movin/pages/product/[Product]/index.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
-import TodaySidebar from "../../../src/components/commons/todaySidebar";
+import TodaySidebar from "../../../src/components/commons/todaySidebar/index";
 import ProductDetail from "../../../src/components/units/product/detail/ProductDetail.container";
 import { FETCH_USED_ITEM } from "../../../src/components/units/product/detail/ProductDetail.queries";
 import ProductCommentList from "../../../src/components/units/productComment/list/ProductCommentList.container";
