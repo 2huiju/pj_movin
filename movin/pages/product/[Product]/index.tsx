8348c4cc4ebedf15ce6c319afd9a15a40b40fd17
heejoo45890 <heejoo45890@gmail.com>
2022-08-25 11:55:43 +0900

    :수정: error

--- movin/pages/product/[Product]/index.tsx
+++ movin/pages/product/[Product]/index.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
+import TodaySidebar from "../../../src/components/commons/todaySidebar";
 import ProductDetail from "../../../src/components/units/product/detail/ProductDetail.container";
 import { FETCH_USED_ITEM } from "../../../src/components/units/product/detail/ProductDetail.queries";
 import ProductCommentList from "../../../src/components/units/productComment/list/ProductCommentList.container";
 import ProductCommentWrite from "../../../src/components/units/productComment/write/ProductCommentWrite.container";
-import TodaySidebar from "../../../src/components/commons/todaySidebar";
 
 export default function ProductDetailPage() {
   const router = useRouter();
